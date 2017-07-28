3e1dfcd0a038d29056b9cfd1f800ebefe48001b5
Rob Murdock <rmurdock@pillartechnology.com>
2017-07-28 14:11:08 -0400

    Added ability to post a new squad to a mongo db.

--- SquadManagerService/service/service.go
+++ SquadManagerService/service/service.go
@@ -3,24 +3,97 @@ package service
 import (
 	"net/http"
 	"encoding/json"
+	"github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
-func MakeMainHandler() http.Handler {
-	return http.HandlerFunc(mainHandler)
+type ServiceConfiguration struct {
+	DatabaseName string
+	Host         string
 }
 
-func mainHandler(writer http.ResponseWriter, request *http.Request) {
-
-	mux := http.NewServeMux()
-
-	mux.HandleFunc("/squad", SquadListHandler)
-
-	mux.ServeHTTP(writer, request)
+type SquadRepository struct {
+	Config  ServiceConfiguration
+	session *mgo.Session
 }
 
-func SquadListHandler(writer http.ResponseWriter, _ *http.Request) {
+func NewSquadRepository(config ServiceConfiguration) (*SquadRepository, error) {
+	session, err := mgo.Dial("mongo,localhost")
+	if err != nil {
+		return nil, err
+	}
 
-	squads := []string{}
+	repository := SquadRepository{config, session}
+
+	return &repository, nil
+}
+
+func MakeMainHandler(config ServiceConfiguration) http.Handler {
+
+	repository, err := NewSquadRepository(config)
+
+	if (err != nil) {
+		panic(err)
+	}
+
+	router := httprouter.New()
+
+	router.GET("/squad", repository.squadListHandler)
+	router.POST("/squad", repository.squadCreateHandler)
+
+	return http.HandlerFunc(router.ServeHTTP)
+}
+
+func (self SquadRepository) squadListHandler(writer http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
+
+	squads, err := self.listSquads()
+	if(err != nil) {
+		writer.WriteHeader(500)
+		return
+	}
+
 	json.NewEncoder(writer).Encode(squads)
 }
 
+func (self SquadRepository) squadCreateHandler(writer http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
+
+	squadId, err := self.addSquad()
+	if (err != nil) {
+		writer.WriteHeader(500)
+		return
+	}
+
+	writer.WriteHeader(202)
+	json.NewEncoder(writer).Encode(squadId)
+}
+
+type SquadDocument struct {
+	ID bson.ObjectId `bson:"_id,omitempty"`
+}
+
+func (self SquadRepository) addSquad() (bson.ObjectId, error) {
+	collection := self.session.DB(self.Config.DatabaseName).C("squad")
+	id := bson.NewObjectId()
+	return id, collection.Insert(SquadDocument{id})
+}
+
+func (self SquadRepository) listSquads() ([]string, error) {
+	collection := self.session.DB(self.Config.DatabaseName).C("squad")
+
+	var squadDocuments []SquadDocument
+	err := collection.Find(bson.M{}).All(&squadDocuments)
+	if(err != nil) {
+		return nil, err
+	}
+
+	return convertSquadDocumentsToIds(squadDocuments), nil
+}
+
+func convertSquadDocumentsToIds(documents []SquadDocument) []string {
+	idList := make([]string, len(documents))
+	for index, document := range documents {
+		idList[index] = document.ID.Hex()
+	}
+	return idList
+}
--- SquadManagerService/service_test/service_test.go
+++ SquadManagerService/service_test/service_test.go
@@ -8,11 +8,37 @@ import (
 	"github.com/robertfmurdock/SquadManager/SquadManagerService/service"
 	"encoding/json"
 	"io"
+	"bytes"
 )
 
 var (
-	mainHandler = service.MakeMainHandler()
+	config = service.ServiceConfiguration{
+		DatabaseName:"SquadManagerTestDB",
+		Host:"localhost",
+	}
+	mainHandler = service.MakeMainHandler(config)
 )
+
+func getPostBody(body interface{}) ([]byte, error) {
+	if value, ok := body.(string); ok {
+		return []byte(value), nil
+	} else {
+		return json.Marshal(body)
+	}
+}
+
+func makePostRequest(t *testing.T, url string, body interface{}) *http.Request {
+	value, err := getPostBody(body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	request, err := http.NewRequest("POST", url, bytes.NewReader(value))
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	return request
+}
 
 func TestNoResponseOnMainUrl(t *testing.T) {
 	request := newRequest(t, "GET", "/", nil)
@@ -31,7 +57,7 @@ func newRequest(t *testing.T, method, urlStr string, body io.Reader) *http.Reque
 	return request;
 }
 
-func TestCallGetSquad(t *testing.T) {
+func performGetSquadList(t *testing.T) []string {
 	request := newRequest(t, "GET", "/squad", nil)
 	recorder := httptest.NewRecorder()
 
@@ -42,8 +68,28 @@ func TestCallGetSquad(t *testing.T) {
 	var loadedJson []string
 	json.Unmarshal(recorder.Body.Bytes(), &loadedJson)
 
-	expectedJson := []string{}
-
-	assert.Equal(t, expectedJson, loadedJson)
+	return loadedJson
 }
 
+func performPostSquad(t *testing.T) *httptest.ResponseRecorder {
+	request := makePostRequest(t, "/squad", "")
+
+	recorder := httptest.NewRecorder()
+
+	mainHandler.ServeHTTP(recorder, request)
+	return recorder
+}
+
+func TestPOSTSquadWillIncludeNewSquadInSubsequentGET(t *testing.T) {
+
+	response := performPostSquad(t)
+
+	assert.Equal(t, http.StatusAccepted, response.Code)
+
+	var newSquadId string
+	json.Unmarshal(response.Body.Bytes(), &newSquadId)
+
+	squadList := performGetSquadList(t)
+
+	assert.Contains(t, squadList, newSquadId);
+}
